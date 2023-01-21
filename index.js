dfe8540ddb19a2d2eee50ac066a124a42e128c33
Nataiia Vodopianova <natalvod@yandex.ru>
2023-01-21 17:10:38 +0300

    Added code js

---
+++ index.js
@@ -0,0 +1,45 @@
+let a = 10;
+alert(a);
+
+let releasIphone = 2007;
+alert(releasIphone);
+
+let javaScriptCreator = 'Brendan Eich';
+alert(javaScriptCreator);
+
+let num1 = 10;
+let num2 = 2;
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 / num2);
+
+let result = 2**5;
+alert(result);
+
+a = 9;
+let b = 2;
+alert(a % b);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+++num;
+--num;
+alert(num);
+
+let age = Number(prompt('Сколько вам лет?'));
+alert(age);
+
+const user = { 
+    name: 'John Smith', 
+    age: 30, 
+    isAdmin: true 
+};
+user['city of residence'] = 'New York';
+user.age = 32;
+delete user['city of residence'];
+let info = prompt('age');
+alert(user[info]);
